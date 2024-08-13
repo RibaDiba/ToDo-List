df0d87a8c0006c470696881b2fcf7117500682fe
Tanish Mittal <mittal.tanish@gmail.com>
2024-08-13 11:16:16 -0400

    better ids and fixed remaining counter

--- src/lib/Task.svelte
+++ src/lib/Task.svelte
@@ -1,74 +1,81 @@
 <script>
-     // @ts-nocheck
+	// @ts-nocheck
 
-    import { createEventDispatcher } from "svelte";
-    const dispatch = createEventDispatcher();
-   
-    export let task;
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
 
-    function updateTask(updated) {
-        task = {...task, ...updated};
-        dispatch('update', task)
-    }
+	export let task;
 
-    function toggle() {
-        updateTask({isDone: task.isDone})
-    }
+	function updateTask(updated) {
+		task = { ...task, ...updated };
+		dispatch('update', task);
+	}
 
-    function remove() {
-        dispatch('remove', task)
-    }
+	function toggle() {
+		updateTask({ isDone: task.isDone });
+		console.log(task);
+	}
 
-    function changeName() {
-        updateTask({name: task.name})
-    }
+	function remove() {
+		dispatch('remove', task);
+	}
+
+	function changeName() {
+		updateTask({ name: task.name });
+	}
 </script>
 
 <div class="input-container">
-    <input type="checkbox" class="done" bind:checked={task.isDone} on:change={toggle}>
-    <input type="text" placeholder="Enter Task" class="name-task" bind:value={task.name} on:change={changeName}>
-    <div class="button-container">
-         <button class="close" on:click={remove}>x</button>
-    </div>
+	<input type="checkbox" class="done" bind:checked={task.isDone} on:change={toggle} />
+	<input
+		type="text"
+		placeholder="Enter Task"
+		class="name-task"
+		bind:value={task.name}
+		on:change={changeName}
+	/>
+	<div class="button-container">
+		<button class="close" on:click={remove}>x</button>
+	</div>
 </div>
 
 <style>
-    .name-task {
-         width: 10rem;
-         margin: 10px;
-         padding: 10px;
-         border-style: none;
-         outline: none;
-         position: relative;
-         font-family: 'Inter';
-    }
+	.name-task {
+		width: 10rem;
+		margin: 10px;
+		padding: 10px;
+		border-style: none;
+		outline: none;
+		position: relative;
+		font-family: 'Inter';
+	}
 
-    .done {
-         width: 20px;
-         height: auto;
-    }
+	.done {
+		width: 20px;
+		height: auto;
+	}
 
-    .close {
-         background-color: inherit;
-         border-style: none;
-         outline-style: none;
-         font-family: "Inter";
-         font-size: 20px;
-         cursor: pointer;
-         transition: .4s;
-    }
+	.close {
+		background-color: inherit;
+		border-style: none;
+		outline-style: none;
+		font-family: 'Inter';
+		font-size: 20px;
+		cursor: pointer;
+		transition: 0.4s;
+	}
 
-    .close:active {
-         font-size: 15px;
-    }
+	.close:active {
+		font-size: 15px;
+	}
 
-    .button-container {
-         display: flex;
-         justify-content: center;
-    }
+	.button-container {
+		display: flex;
+		justify-content: center;
+	}
 
-    .input-container {
-         display: flex;
-         justify-items: center;
-    }
+	.input-container {
+		display: flex;
+		justify-items: center;
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,118 +1,112 @@
 <script lang="ts">
-    //@ts-nocheck
+	//@ts-nocheck
 
-    import Task from "$lib/Task.svelte";
-    let tasks: any[] = []
+	import Task from '$lib/Task.svelte';
+	let tasks: any[] = [];
 
-    let remaining = 0
+	let remaining = 0;
+	let newTaskID;
 
-    $: remaining = tasks.length
+	
+	$: remaining = tasks.filter((task) => !task.isDone).length;
 
-    function handleTask() {
-         tasks = [
-              {
-                   name: "",
-                   isDone: false,
-                   id: tasks.length
-              }, ...tasks
-         ]
+	function handleTask() {
+		tasks = [
+			{
+				name: '',
+				isDone: false,
+				id: Math.random()
+			},
+			...tasks
+		];
 
-         console.log(tasks)
-    }
+		console.log(tasks);
+	}
 
-    function handleDelete(task) {
-         tasks = tasks.filter(item => item.id !== task.id);
-    }
+	function handleDelete(task) {
+		tasks = tasks.filter((item) => item.id !== task.id);
+	}
 
-    function handleChecked() {
-         tasks.forEach((task) => {
-              if (task.isDone) {
-                   remaining--
-              }
-          })
-    }
+	function updateTask(task) {
+		const i = tasks.findIndex((t) => t.id === task.id);
+		tasks[i] = { ...tasks[i], ...task };
+	}
 
-    function updateTask(task) {
-        const i = tasks.findIndex((t) => t.id === task.id);
-        tasks[i] = { ...tasks[i], ...task };
-    }
-
-    function removeCompleted() {
-        tasks = tasks.filter(item => !item.isDone);
-    }
+	function removeCompleted() {
+		tasks = tasks.filter((item) => !item.isDone);
+	}
 </script>
 
 <main>
-    <div class="todo-list-container">
-         <h1 class="title">Todo List</h1>
+	<div class="todo-list-container">
+		<h1 class="title">Todo List</h1>
 
-         <ul class="task-list">
-              {#each tasks as task}
-                   <Task task={task} on:remove={(e) => handleDelete(e.detail)} on:update={(e) => updateTask(e.detail)}/>
-              {/each}
-         </ul>
+		<ul class="task-list">
+			{#each tasks as task}
+				<Task
+					{task}
+					on:remove={(e) => handleDelete(e.detail)}
+					on:update={(e) => updateTask(e.detail)}
+				/>
+			{/each}
+		</ul>
 
-         <div class="divider"></div>
+		<div class="divider"></div>
 
-         <p class="remaining">Tasks Remaining: {remaining}</p>
-         <button class="action-button" on:click={handleTask}>Add Task</button>
-        <button class ="action-button" on:click={removeCompleted}>Remove Completed</button>
-    </div>
+		<p class="remaining">Tasks Remaining: {remaining}</p>
+		<button class="action-button" on:click={handleTask}>Add Task</button>
+		<button class="action-button" on:click={removeCompleted}>Remove Completed</button>
+	</div>
 </main>
 
 <style>
+	.title {
+		font-family: 'Inter';
+	}
 
-    .title {
-         font-family: 'Inter';
-    }
-    
-    .todo-list-container {
-         margin-left: 10rem;
-         margin-right: 10rem;
-         margin-top: 5rem;
-    }
+	.todo-list-container {
+		margin-left: 10rem;
+		margin-right: 10rem;
+		margin-top: 5rem;
+	}
 
-    
-    .action-button {
-         outline-style: none;
-         border-style: solid;
-         border-radius: 4px;
-         background-color: black;
-         color: white;
-         padding: 10px;
-         border-color: black;
-         border-width: 3px;
-         transition: .4s;
-    }
+	.action-button {
+		outline-style: none;
+		border-style: solid;
+		border-radius: 4px;
+		background-color: black;
+		color: white;
+		padding: 10px;
+		border-color: black;
+		border-width: 3px;
+		transition: 0.4s;
+	}
 
-    .action-button:hover {
-         background-color: white;
-         color: black;
-         cursor: pointer;
-    }
+	.action-button:hover {
+		background-color: white;
+		color: black;
+		cursor: pointer;
+	}
 
-    .action-button:active {
-         padding: 5px;
-    }
+	.action-button:active {
+		padding: 5px;
+	}
 
-    
-    .task-list {
-         display: flex;
-         flex-direction: column;
-         padding: 0px;
-    }
+	.task-list {
+		display: flex;
+		flex-direction: column;
+		padding: 0px;
+	}
 
-    .divider {
+	.divider {
+		background-color: whitesmoke;
+		width: auto;
+		height: 5px;
+		margin-bottom: 30px;
+		margin-top: 30px;
+	}
 
-         background-color: whitesmoke;
-         width: auto;
-         height: 5px;
-         margin-bottom: 30px;
-         margin-top: 30px;
-    }
-
-    .remaining {
-         font-family: 'Inter';
-    }
-
+	.remaining {
+		font-family: 'Inter';
+	}
 </style>
